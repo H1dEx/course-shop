5fc403531d73d7a89320b0ad8bb99c84e7930628
H1dEx <hidex404@gmail.com>
2020-12-30 10:19:14 +0300

    add sequelize cli

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {body, validationResult} = require('express-validator/check');
 const cors = require("cors");
 
 const app = express();
@@ -20,7 +21,19 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-const PORT = process.env.PORT || 8080;
+app.post("sign-up",[
+	body('email').isEmail().normalizeEmail(),
+	body('password').not().isEmpty().trim().isLength({min: 5}).withMessage('must be at least 5 chars long')
+		.matches()
+], (req, res, next)=>{
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({errors: errors.array()})
+	}
+	res.send(req.body)
+})
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
